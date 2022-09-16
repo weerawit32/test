d2c079a2f7757b105fc2ad8d45ac11550aebdf50
weerawit32 <vasthbo@hotmail.com>
2022-09-16 14:42:32 +0700

    initial commit

---
+++ test/src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+import { useCallback, useState} from 'react'
+
+function App() {
+  const data = new Array(10).fill(null);
+  const [indexOfItem, setIndexOfIItem] = useState(0);
+
+  const onRender = useCallback(() => {
+    console.log(data)
+  },[data])
+
+  onRender();
+
+
+  return (
+    <div className="App">
+      <ul>
+      {data.map((item, index) => {
+
+        return <Item key={index} index={index} indexOfItem={indexOfItem} setIndexOfIItem={setIndexOfIItem}/>
+      })
+      }
+
+      </ul>
+    </div>
+  );
+}
+
+const Item = ({index, indexOfItem, setIndexOfIItem}) => {
+
+  return <li onClick={()=> setIndexOfIItem(index)}>{indexOfItem === index ? "selected": index}</li>
+}
+
+export default App;
